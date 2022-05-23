f8b46eb1b4cb5dfa16b29aa729c45735d06c8796
Gabriel <gabriel@sereno.com.br>
2022-05-23 12:08:40 -0300

    Algumas implementações no Usuario

--- main/src/Usuário.java
+++ main/src/Usuário.java
@@ -1,2 +1,41 @@
+import Evento.Evento;
+
+import java.util.ArrayList;
+
 public class Usuário {
+
+    String cpf;
+    float saldo;
+
+    // O usuário só pode se inscrever em 5 eventos por CPF!
+
+    ArrayList<Evento> lista_de_eventos = new ArrayList<Evento>(5);
+
+    public Usuário(String cpf, float saldo)
+    {
+        this.saldo = saldo;
+        this.cpf = cpf;
+    }
+
+
+    public void add_to_lista_de_eventos(Evento evento)
+    {
+        if(lista_de_eventos.size() == 5)
+        {
+            throw new IndexOutOfBoundsException("Vocẽ já está inscrito em muitos eventos!");
+        }
+        else {
+            lista_de_eventos.add(evento);
+        }
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
 }
