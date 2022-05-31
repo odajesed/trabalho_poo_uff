647569d9c3a9746d7fedc7524c3e7661f5e589ba
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2022-05-31 13:36:29 -0300

    Adicionados atributos à classe Jogo

--- main/src/Evento/Jogo/Jogo.java
+++ main/src/Evento/Jogo/Jogo.java
@@ -1,9 +1,70 @@
 package Evento.Jogo;
 
 import Evento.Evento;
-// METODO PRA VER SE O JOGO JA PASSOU, O RESULTADO
+
 public class Jogo extends Evento {
+    private Time time1;
+    private Time time2;
+    private int golsTime1;
+    private int golsTime2;
+    
     public Jogo(String tipo, int numero_maximo, float preco, float duracao, String data,int horario,String status) {
         super(tipo, numero_maximo, preco, duracao, data,horario,status);
+    }
+
+    /**
+     * @return the time1
+     */
+    public Time getTime1() {
+        return time1;
+    }
+
+    /**
+     * @param time1 the time1 to set
+     */
+    public void setTime1(Time time1) {
+        this.time1 = time1;
+    }
+
+    /**
+     * @return the time2
+     */
+    public Time getTime2() {
+        return time2;
+    }
+
+    /**
+     * @param time2 the time2 to set
+     */
+    public void setTime2(Time time2) {
+        this.time2 = time2;
+    }
+
+    /**
+     * @return the golsTime1
+     */
+    public int getGolsTime1() {
+        return golsTime1;
+    }
+
+    /**
+     * @param golsTime1 the golsTime1 to set
+     */
+    public void setGolsTime1(int golsTime1) {
+        this.golsTime1 = golsTime1;
+    }
+
+    /**
+     * @return the golsTime2
+     */
+    public int getGolsTime2() {
+        return golsTime2;
+    }
+
+    /**
+     * @param golsTime2 the golsTime2 to set
+     */
+    public void setGolsTime2(int golsTime2) {
+        this.golsTime2 = golsTime2;
     }
 }
