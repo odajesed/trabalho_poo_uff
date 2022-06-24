c45dd3f0115c3d522f60bf8cfb5f1ef02c41a66a
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2022-06-24 18:12:28 -0300

    testa novas funções do usuário

--- main/src/Main.java
+++ main/src/Main.java
@@ -9,19 +9,20 @@ import Evento.Gerenciador_Palestra;
 public class Main {
     public static void main(String[] args) throws Exception {
         Artista a1 = new Artista("a","a",13);
-        Evento s1 = new Show("a",5,500,1,"a",1,a1,"A");
+        Evento s1 = new Show("show 1","a",5,500,1,"a",1,a1,"A");
 
         Palestrante p1 = new Palestrante("a","a");
-        Evento pale1 = new Palestra("a",5,1,1,"a",1,"a",p1,"Acabou");
+        Evento pale1 = new Palestra("palestra 1","a",5,1,1,"a",1,"a",p1,"Acabou");
 
         Usuário u1 = new Usuário("11",10000);
 
         Gerenciador_Palestra.fim_palestra(pale1);
         u1.compra_evento(s1);
-        u1.imprimir_lista();
-
-
-
+        u1.compra_evento(s1);
+        u1.compra_evento(pale1);
+        u1.compra_evento(s1);
+        u1.compra_evento(pale1);
+        u1.imprimir_historico();
 
     }
 }
