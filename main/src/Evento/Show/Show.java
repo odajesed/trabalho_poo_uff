061a89c96df9e9919e598948e72170ea37b9e3aa
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2022-06-24 18:09:33 -0300

    add atributo nome ao construtor

--- main/src/Evento/Show/Show.java
+++ main/src/Evento/Show/Show.java
@@ -4,8 +4,8 @@ import Evento.Evento;
 
 public class Show extends Evento {
     Artista artista;
-    public Show(String tipo, int numero_maximo, float preco, float duracao, String data,int horario, Artista artista,String status) {
-        super(tipo, numero_maximo, preco, duracao, data,horario,status);
+    public Show(String nome,String tipo, int numero_maximo, float preco, float duracao, String data,int horario, Artista artista,String status) {
+        super(nome,tipo, numero_maximo, preco, duracao, data,horario,status);
         this.artista = artista;
     }
 
